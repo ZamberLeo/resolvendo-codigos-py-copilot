5a0e7afa13b8cef9114d9b2b07701667946e3328
LeoZ Developer <zamberac@gmail.com>
2025-12-02 13:38:26 -0300

    Primeira parte do desafio

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+dados_concatenados = info1 + " " + info2
+
+print("Dados concatenados:", dados_concatenados)
