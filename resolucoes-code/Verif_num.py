f5928e072ca022f0137aa23054f233023bff94b1
LeoZ Developer <zamberac@gmail.com>
2025-12-02 14:11:20 -0300

    Quarto Desafio

---
+++ resolucoes-code/Verif_num.py
@@ -0,0 +1,8 @@
+# Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. Uma dica é: Utilize condicionais para realizar a verificação e, se possível, faça uso do Github Copilot(ou outra IA) para otimizar a estrutura do código.
+
+num = int(input("Digite um número inteiro: "))
+
+if num % 2 == 0:
+    print(f"{num} é par")
+else:
+    print(f"{num} é ímpar")
